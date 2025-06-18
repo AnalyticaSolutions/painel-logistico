f4639a40c2e8dfa153156673a46c881888297816
Analytica Solutions <pythondevs2022@gmail.com>
2025-06-17 22:32:27 -0300

    Update app.py
    Corrigindo erro de exibição de do dashboard no arquivo app.py

--- app.py
+++ app.py
@@ -10,22 +10,23 @@ from evolucao_temporal import render_temporal
 # Configuração da página
 st.set_page_config(page_title="Painel Logístico", layout="wide")
 
-# Escopo necessário para acessar o Google Sheets
+# Autenticação Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_json = st.secrets["google_service_account"]  # já é um dicionário válido
+creds_json = st.secrets["google_service_account"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
 client = gspread.authorize(creds)
 
 # Nome da planilha e aba
-SHEET_NAME = "Planilha_KPIs_Coordenadores RJ MG ES SP JUNHO"
-worksheet = client.open(SHEET_NAME).sheet1
-
-# Lê os dados e transforma em DataFrame
+spreadsheet_name = "Planilha_KPIs_Coordenadores RJ MG ES SP JUNHO"
+worksheet_name = "Base_Tratada"
 sheet = client.open(spreadsheet_name).worksheet(worksheet_name)
-data = worksheet.get_all_records()
+
+# Leitura dos dados
+data = sheet.get_all_records()
 df = pd.DataFrame(data)
 
-st.sidebar.title("📊 Navegação")
+# Navegação
+st.sidebar.title("🧭 Navegação")
 pagina = st.sidebar.radio("Escolha a página:", ["Dashboard Geral", "Evolução Temporal"])
 
 if pagina == "Dashboard Geral":
